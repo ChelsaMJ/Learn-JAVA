ecd5d428949b69bd955531128ec2213ddac2d000
Chelsa MJ <chelsamj2004@gmail.com>
2025-10-13 04:15:25 +0530

    Oct13 4:15

--- fileRead.java
+++ fileRead.java
@@ -1,8 +1,27 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class fileRead {
     public static void main(String[] args) { 
 
         String filePath ="D:\\Learn JAVA\\output2.txt";
 
+        try(BufferedReader reader = new BufferedReader(new FileReader(filePath))){
+            // read file line by line
+            String line;
+
+            while((line = reader.readLine()) != null){ // continue reading lines until null
+                System.out.println(line);
+            }
+        }
+        catch(FileNotFoundException e){
+            System.out.println("File not found");
+        }
+        catch (IOException e) { 
+            System.out.println("Could not write file");
+        }
 
     }
 
